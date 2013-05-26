08e9a674f3a421453d3d7e219c55eb4898a2da10
Robert Kajic <robert@kajic.com>
2013-05-26 12:57:04 +0200

    Resolve monitored apps at initialization
    Add vpn and is_vpn_connected methods

--- vpn-monitor.py
+++ vpn-monitor.py
@@ -19,9 +19,9 @@ from appscript import app
 from docopt import docopt
 
 class VpnMonitor(object):
-  def __init__(self, vpn, apps):
-    self.vpn = vpn
-    self.apps = apps
+  def __init__(self, vpn_name, app_names):
+    self.vpn_name = vpn_name
+    self.apps = [(name, app(name)) for name in app_names]
 
   def run(self):
     try:
@@ -34,32 +34,34 @@ class VpnMonitor(object):
 
   def main_loop(self):
     while True:
-      vpn = app('System Events').network_preferences.services[self.vpn]
-      vpn_connected = vpn.current_configuration.connected()
+      if not self.is_vpn_connected():
 
-      if not vpn_connected:
         self.quit_apps()
 
         if self.is_wifi_connected():
-          vpn.connect()
           sleep(30)
+          self.vpn().connect()
       else:
         self.run_apps()
       
       sleep(0.1)
 
+  def vpn(self):
+    return app('System Events').network_preferences.services[self.vpn_name]
+
+  def is_vpn_connected(self):
+    return self.vpn().current_configuration.connected()
+
   def is_wifi_connected(self):
     return 'Wi-Fi Power (en1): On' in check_output(['networksetup', '-getairportpower', 'en1'])
 
   def quit_apps(self):
-    for name in self.apps:
-      cur = app(name)
+    for name, cur in self.apps:
       if cur.isrunning():
         cur.quit()
 
   def run_apps(self):
-    for name in self.apps:
-      cur = app(name)
+    for name, cur in self.apps:
       if not cur.isrunning():
         cur.run()
       
